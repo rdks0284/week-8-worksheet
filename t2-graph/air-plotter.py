169c8c65fe967eab1a0c8a8617d5f08a675878b5
Hashim [rdks0284] <rdks0284@leeds.ac.uk>
2025-12-04 16:41:11 +0000

    end of worksheet 9

--- t2-graph/air-plotter.py
+++ t2-graph/air-plotter.py
@@ -11,25 +11,29 @@ You would plot a point at (68+20+25+5)/4 = 29.5
 
 The X-axis should be the date, the Y-axis should be the average pollution.
 '''
+'''Student Name: Hashim Ali
+Student ID: 201967833'''
 
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
-
 x = []
 y = []
 
+# Read CSV file
 data = pd.read_csv("leeds-centre-air-quality.csv", header=None, skiprows=1)
+# Iterate through each row in CSV file
 for i, row in data.iterrows():
     if row.isnull().all():
         break
     x.append(row[0])
+    # Calculate average of 4 values
     y.append((int(row[1]) + int(row[2]) + int(row[3]) + int(row[4]))/4)
 
+# Plot the graph
 plt.plot(x, y)
 
-plt.title("Student Name: Hashim Ali")
+plt.title("Student Name: Hashim Ali | rdks0284 | 201967833")
 plt.xlabel("date")
 plt.ylabel("average pollution")
 plt.savefig("PLOT.png")
